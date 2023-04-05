4a4ca6a97895343855dac37c43468647f65e2d57
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-04-05 14:05:38 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
 
 
   return (
     <div>
-      <Post
-        author="Henrique Vazquez"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis sunt, nam veritatis velit architecto voluptatem et amet dolores, alias repellat molestias sed itaque nesciunt optio possimus. Unde minus enim eligendi?"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Henrique Vazquez"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis sunt, nam veritatis velit architecto voluptatem et amet dolores, alias repellat molestias sed itaque nesciunt optio possimus. Unde minus enim eligendi?"
+          />
 
-      <Post
-        author="Karen Vazquez"
-        content="Amo a Sophia"
-      />
-
+          <Post
+            author="Karen Vazquez"
+            content="Amo a Sophia"
+          />
+        </main>
+      </div>
     </div>
   )
 }
