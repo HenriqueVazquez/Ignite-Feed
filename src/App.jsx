223cc26c5bebd5b75b8be44ba35cc14967263f35
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-04-05 08:22:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Post } from './Post';
+
+export function App() {
+
+
+  return (
+    <div>
+      <Post
+        author="Henrique Vazquez"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis sunt, nam veritatis velit architecto voluptatem et amet dolores, alias repellat molestias sed itaque nesciunt optio possimus. Unde minus enim eligendi?"
+      />
+
+      <Post
+        author="Karen Vazquez"
+        content="Amo a Sophia"
+      />
+
+    </div>
+  )
+}
+
+
